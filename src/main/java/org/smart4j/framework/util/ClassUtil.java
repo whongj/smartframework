9f94184ac94af66ba68765647dbf9f5e5cb2d629
whongj <498816896@qq.com>
2017-08-26 21:11:37 +0800

    Initial commit

---
+++ src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -0,0 +1,81 @@
+package org.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Created by ${wanghongjie}  on 2017/8/19.
+ */
+public final class ClassUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
+
+    /**
+     * 获取类加载器
+     */
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * 加载类
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className, isInitialized, getClassLoader());
+
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("load class failure", e);
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    /**
+     * 获取指定包名下的所有类
+     */
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")) {
+                        String packagePath = url.getPath().replaceAll("%20", " ");
+                        addClass(classSet, packagePath, packageName);
+
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if (jarFile != null) {
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if (jarEntryName.endsWith(".class")) {
+                                        String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/", ".");
+
+                                    }
+
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ src/main/java/org/smart4j/framework/util/StringUtil.java
@@ -0,0 +1,27 @@
+package org.smart4j.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**字符串工具类
+ * Created by ${wanghongjie} on 2017/8/19.
+ */
+public class StringUtil {
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String string) {
+        if (string != null) {
+            string = string.trim();
+
+        }
+        return StringUtils.isEmpty(string);
+    }
+
+    /**
+     * 判断字符串是否非空
+     */
+    public static boolean isNotEmpty(String string) {
+        return !isEmpty(string);
+    }
+
+}
